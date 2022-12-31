c916e10cf1e3baa0279a38a863520c17b8acc2ba
corrado.michele2002 <corrado.michele2002@gmail.com>
2022-12-31 14:17:06 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClassificheComponent } from './classifiche/classifiche.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { InserisciTempoComponent } from './inserisci-tempo/inserisci-tempo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClassificheComponent,
+    CategorieComponent,
+    UserDashboardComponent,
+    InserisciTempoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/inserisci-tempo/inserisci-tempo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inserisci-tempo',
+  templateUrl: './inserisci-tempo.component.html',
+  styleUrls: ['./inserisci-tempo.component.css']
+})
+export class InserisciTempoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
