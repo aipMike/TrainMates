8877a1cec24fee781d4b15081f8a2d6cb5c92e0e
corrado.michele2002 <corrado.michele2002@gmail.com>
2023-01-02 13:24:57 +0100

    fix a links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClassificheComponent } from './classifiche/classifiche.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { InserisciTempoComponent } from './inserisci-tempo/inserisci-tempo.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClassificheComponent,
+    CategorieComponent,
+    UserDashboardComponent,
+    InserisciTempoComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
